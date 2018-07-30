19870ab8281baa39637c831cd8735706ab6dfd55
Karen C <chenyanhuan66@163.com>
2018-07-30 08:01:34 +0000

    automatically backed up by learn

--- lib/cruise_ship.rb
+++ lib/cruise_ship.rb
@@ -1,10 +1,9 @@
-# passengers = {
-# suite_a: "Amanda Presley", 
-# suite_b: "Seymour Hoffman", 
-# suite_c: "Alfred Tennyson", 
-# suite_d: "Charlie Chaplin", 
-# suite_e: "Crumpet the Elf"
-# }
+passengers = {
+suite_a: "Amanda Presley",
+suite_b: "Seymour Hoffman",
+suite_c: "Alfred Tennyson",
+suite_e: "Crumpet the Elf"
+}
 
 def select_winner(passengers)
   winner = ""
@@ -12,6 +11,6 @@ def select_winner(passengers)
     if suite == :suite_a && name.start_with?("A")
       winner = name
     end
-  end 
-  winner 
+  end
+  winner
 end
